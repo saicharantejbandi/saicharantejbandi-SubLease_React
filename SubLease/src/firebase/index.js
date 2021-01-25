725b3ab54d5cf5618962cfdaf4120bf8994bf53d
saicharantejbandi <52318138+saicharantejbandi@users.noreply.github.com>
2021-01-24 17:50:35 -0700

    Update index.js

--- SubLease/src/firebase/index.js
+++ SubLease/src/firebase/index.js
@@ -1,14 +1,14 @@
 import firebase from 'firebase/app';
 import 'firebase/storage';
 var firebaseConfig = {
-    apiKey: "AIzaSyAensdze6OcZoGgKTz7PcTOgEm8uroDU8Y",
-    authDomain: "react-my-burger-4ff3d.firebaseapp.com",
-    databaseURL: "https://react-my-burger-4ff3d.firebaseio.com",
-    projectId: "react-my-burger-4ff3d",
-    storageBucket: "react-my-burger-4ff3d.appspot.com",
-    messagingSenderId: "373716093509",
-    appId: "1:373716093509:web:97f91001439318cff0712c",
-    measurementId: "G-XDBMCFPWCD"
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
+    projectId: :"",
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: "",
+    measurementId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
